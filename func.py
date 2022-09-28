f34d0915fde73d861c899685b7f0861a3e36eb15
Kroonsz <Kroonszq@gmail.com>
2022-09-28 09:21:03 +0200

    change structure

--- func.py
+++ func.py
@@ -10,7 +10,7 @@ def printTxt(text):
     for x in text.read():
         sys.stdout.write(x)
         sys.stdout.flush()
-        time.sleep(0.05) if texttype == "typing" else None # sleeps for 5 seconds
+        time.sleep(0.01) if texttype == "typing" else None # sleeps for 5 seconds
     print("\n")
 
 def playsound(path, action):
@@ -20,3 +20,18 @@ def playsound(path, action):
         mixer.music.load(path)
         mixer.music.play(-1) # loop ifinity
         mixer.music.set_volume(0.1)
+
+def inputfield(txt, choises, inv):
+    while True:
+        print("What are you gonna do? ")
+        opt = ord("A")
+        for x in choises:
+            print(f"{chr(opt)} - {x}")
+            opt+=1
+        playerInput = input(f"\033[91m\033[1m{txt}\033[0m")
+        if playerInput == "inventory":
+            print(inv)
+        else:
+            return playerInput
+
+
--- program.py
+++ program.py
@@ -1,44 +1,19 @@
-from func import playsound, printTxt, os
+from chapter1 import chapter1
+from intro import intro
 
-introtxt = open("text/chapter1/intro.txt", "r", encoding="utf-8")
-farmtxt = open("text/chapter2/farm.txt", "r", encoding="utf-8")
+chapter = 0
+playing = True
 items = []
 
-
-# - CHAPTER 1 INTRODUCTION ------------------------------------------------------------------------------------------------
-playsound("sound/intromusic.wav", "start")                    #play song
-print("\033[35m\033[1mCHAPTER 1 - introduction\033[0m")       #display chapter
-printTxt(introtxt)                                            #display chapter txt
-
-# player desicion
-ready = input("\033[91m\033[1mARE YOU READY TO START THIS ADVENTURE? (YES / NO): \033[0m")
-if ready != "YES":
-    exit("Not ready? No problem come back next time")
-
-playsound("", "stop")                                         #stop song
-os.system("cls")                                              #clear cls
-
-
-playing = True
 while playing:
-    # - CHAPTER 2 THE FARM ------------------------------------------------------------------------------------------------
-    playsound("sound/naturesound.wav", "start")               #play song
-    print("\033[35m\033[1mCHAPTER 1 - the farm\033[0m")       #display chapter
-    printTxt(farmtxt)                                         #display chapter txt
-
-    # player desicion
-    while True:
-        decision1 = input("DO YOU WANT TO CHECK YOUR CHICKENS BEFORE HEADING TO THE VILLAGE?(YES/NO): ")
-        if decision1.upper() == "YES":
-            items.append("golden egg")
-            print("Nor checks the chickens and finds a golden egg and puts it in his bag")
-            break
-        elif decision1.upper() == "CHICKENS":
-            break
+    if chapter == 0:                                        #load in intro
+        intr = intro()
+        if intr:
+            chapter+=1
         else:
-            print("invalid action")
-    print("Norr walks with the horses to the village")
-    input()
-   
+            continue
 
+    if chapter == 1:                                        #load in chapter1
+        chap1 = chapter1(items)
 
+    input("YESSSS")

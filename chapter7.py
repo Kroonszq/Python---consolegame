3349f997177fbdbedca7b9fef28d262c80696577
Kroonsz <Kroonszq@gmail.com>
2022-10-03 10:29:17 +0200

    add puzzle

--- chapter7.py
+++ chapter7.py
@@ -1,4 +1,4 @@
-from func import playsound, printTxt, os, inputfield
+from func import playsound, printTxt, os, inputfield, time
 
 def chapter7(items):
     banditxt = open("text/chapter7/chapter7.txt", "r", encoding="utf-8")         #load chapter txt (primary)         
@@ -18,7 +18,7 @@ def chapter7(items):
 
     
     while True:
-        awnser = inputfield("What will Norr do? ", ["Pass trough", extraOption1, extraOption2])
+        awnser = inputfield("What will Norr do? ", ["Pass trough", "solve puzzle", extraOption1, extraOption2])
         # OPTION A - Pass trough
         if awnser == "A":
             print("2 mortage items get stolen")
@@ -27,9 +27,48 @@ def chapter7(items):
             items.remove("bread")
             items.remove("grain")
             break
-        if extraOption1 or extraOption2:
+        elif awnser == "B":
+            print("Alright i got a puzzle for you.")
+            print("Press enter to start the puzzle... ")
+            valid = True
+            while True:
+                try:
+                    # question 1
+                    question1 = int(input("Whats the fith primary number? "))
+                    if question1 != 11:
+                        valid = False
+                        break
+                    #question 2
+                    question2 = int(input("Whats the third primary number? "))
+                    if question2 != 5:
+                        valid = False
+                        break
+                    #question 3
+                    question3 = int(input("Whats the fourth primary number? "))
+                    if question3 != 7:
+                        valid = False
+                        break
+
+                    break
+                except ValueError:
+                    print("Ha you are more dumber then i thought!!")
+                    valid = False
+                    break
+            if valid:
+                print()
+                print("Well done boy, i will let you pass without harming you!.")
+                #true
+            else:
+                print()
+                print("Ha, bad day for you you failed the puzzle and i will rob you.")
+                print(f"\033[31m\033[1mlost bread & grain\033[0m")
+                items.remove("bread")
+                items.remove("grain")
+                #false
+            break
+        elif extraOption1 or extraOption2:
             # OPTION B - gplden egg/magic apple
-            if awnser == "B":
+            if awnser == "C":
                 if extraOption1:
                     print("Norr gives the golden egg to the badints to buy them off the bandits accept and let him trough")
                     items.remove("golden egg")
@@ -38,7 +77,7 @@ def chapter7(items):
                     items.remove("apple")
                 break
             # OPTION C - golden magic apple
-            elif awnser == "C":
+            elif awnser == "D" and extraOption2:
                 print("The magic apple drops from Norr his pocket and out of nowhere out of the magic apple spawns a giant tree and norr and his horse are liftid up")
                 items.remove("apple")
                 break
